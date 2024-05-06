05e843063d11e2ff181ddf8bb8c1031d19eaaf2f
SergeyGitAccount <l_w_d@i.ua>
2024-05-06 23:42:58 +0300

    Initiated the project

---
+++ tests/example.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+
+// ! ******************************************************************************
+// test.skip('has titleSecond', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test.only('has titleThird', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright111/);
+// });
+
+
+// test.only('has titleFourth', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+
