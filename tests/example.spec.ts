deecf73f7e1f8957472c86b6e22ef1b5cd735bbd
SergeyGitAccount <l_w_d@i.ua>
2024-05-08 01:09:14 +0300

    initial commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -23,7 +23,7 @@ test('get started link', async ({ page }) => {
 test.skip('has titleSecond', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
+  // TODO Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
